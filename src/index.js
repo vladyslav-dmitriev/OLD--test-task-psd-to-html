9a0da0f7263e385660cd7d4b25f4654e42378e67
Vladyslav <vladyslav.dmitriev@gmail.com>
2018-02-24 22:29:01 +0200

    first part is made

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import "bootstrap";
 import 'font-awesome/scss/font-awesome.scss'
 import './sass/main.sass';
 
-console.log('in index.js');
-console.log($);
-console.log(jQuery);
+$(document).ready(function() {
+	$('#accordion .accordion__title').on('click', f_accordion);
+	$('#accordion .accordion__title').on('click', function() {
+		$('#accordion .accordion__title').not($(this)).removeClass('accordion__title_active');
+		$(this).addClass('accordion__title_active');
+	});
+});
+ 
+function f_accordion(){
+	$('#accordion .accordion__text').not($(this).next()).slideUp(400);
+	$(this).next().slideToggle(400);
+	$('#accordion .accordion__title .fas').not($(this)).removeClass('fa-caret-up');
+	$(this).find('i').addClass('fa-caret-up');
+};
+
+$(document).ready(function() {
+	$('#accordion .accordion__title')[0].click();
+});
