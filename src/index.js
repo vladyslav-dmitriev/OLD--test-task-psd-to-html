046863bc6618b1646d9c8e9de025db081ab6b784
Vladyslav <vladyslav.dmitriev@gmail.com>
2018-02-25 11:38:03 +0200

    add owl.carousel

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap";
-import 'font-awesome/scss/font-awesome.scss'
+import 'bootstrap';
+import 'font-awesome/scss/font-awesome.scss';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel';
 import './sass/main.sass';
 
-$(document).ready(function() {
-	$('#accordion .accordion__title').on('click', f_accordion);
-	$('#accordion .accordion__title').on('click', function() {
-		$('#accordion .accordion__title').not($(this)).removeClass('accordion__title_active');
-		$(this).addClass('accordion__title_active');
-	});
-});
- 
-function f_accordion(){
-	$('#accordion .accordion__text').not($(this).next()).slideUp(400);
-	$(this).next().slideToggle(400);
-	$('#accordion .accordion__title .fas').not($(this)).removeClass('fa-caret-up');
-	$(this).find('i').addClass('fa-caret-up');
-};
 
-$(document).ready(function() {
-	$('#accordion .accordion__title')[0].click();
+
+$(document).ready(function(){
+  	$('.owl-carousel').owlCarousel({
+  		nav: true,
+  		margin: 18,
+  		responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+            0: {
+				items:1
+			},
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+  	});
 });
+
+
